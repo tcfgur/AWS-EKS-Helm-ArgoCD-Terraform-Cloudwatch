d2855bfd18ed109a19677128df2984671a7181b8
tcfgur <fatihgur1991@gmail.com>
2024-04-29 13:45:26 +0300

    update argocd job

--- Terraform/deployment.tf
+++ Terraform/deployment.tf
@@ -1,27 +1,27 @@
-	resource "kubernetes_manifest" "backend-argocd-job" {
- 
-
-  manifest  = yamldecode(file("../ArgoCD/backendjob.yaml"))
-
-}
-
-#resource "kubernetes_manifest" "frontend-argocd-job" {
+#	resource "kubernetes_manifest" "backend-argocd-job" {
 # 
 #
-#  manifest  = yamldecode(file("../ArgoCD/frontendjob.yaml"))
+#  manifest  = yamldecode(file("../ArgoCD/backendjob.yaml"))
+#
 #}
-
-resource "kubernetes_manifest" "repo-argocd-job" {
- 
-
-  manifest  = yamldecode(file("../ArgoCD/repo.yaml"))
-}
-
-resource "kubernetes_manifest" "serviceaccount-argocd-job" {
- 
-
-  manifest  = yamldecode(file("../ArgoCD/serviceaccount.yaml"))
-}
+#
+##resource "kubernetes_manifest" "frontend-argocd-job" {
+## 
+##
+##  manifest  = yamldecode(file("../ArgoCD/frontendjob.yaml"))
+##}
+#
+#resource "kubernetes_manifest" "repo-argocd-job" {
+# 
+#
+#  manifest  = yamldecode(file("../ArgoCD/repo.yaml"))
+#}
+#
+#resource "kubernetes_manifest" "serviceaccount-argocd-job" {
+# 
+#
+#  manifest  = yamldecode(file("../ArgoCD/serviceaccount.yaml"))
+#}
 
 resource "helm_release" "metrics_server" {
   name       = "metrics-server"
