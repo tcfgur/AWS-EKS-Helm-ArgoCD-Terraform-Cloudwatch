86dbf2bdf59ec08d034d0db8882cd5a4c3e99e19
tcfgur <fatihgur1991@gmail.com>
2024-05-16 23:34:57 +0300

    update cwalert

--- Terraform/cwalert.tf
+++ Terraform/cwalert.tf
@@ -2,9 +2,9 @@ resource "aws_cloudwatch_metric_alarm" "eks_cpu_alarm" {
   alarm_name          = "eks-cpu-alarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 2
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EKS"
-  period              = 300
+  metric_name         = "pod_cpu_utilization_over_pod_limit"
+  namespace           = "ContainerInsights"
+  period              = 15
   statistic           = "Average"
   threshold           = 80
   alarm_description   = "Alarm when CPU exceeds 80% for 2 consecutive periods"
@@ -14,18 +14,21 @@ resource "aws_cloudwatch_metric_alarm" "eks_cpu_alarm" {
   }
 }
 
+
 resource "aws_cloudwatch_metric_alarm" "eks_memory_alarm" {
   alarm_name          = "eks-memory-alarm"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 2
-  metric_name         = "MemoryUtilization"
-  namespace           = "AWS/EKS"
-  period              = 300
+  evaluation_periods  = 1
+  metric_name         = "pod_memory_utilization"
+  namespace           = "ContainerInsights"
+  period              = 15
   statistic           = "Average"
-  threshold           = 80
-  alarm_description   = "Alarm when memory exceeds 80% for 2 consecutive periods"
+  threshold           = 1
+  alarm_description   = "Alarm when memory exceeds 80% for 1 datapoint within 5 minutes"
   alarm_actions       = [aws_sns_topic.eks_alerts_topic.arn] 
   dimensions = {
-    ClusterName = aws_eks_cluster.my_cluster.name 
+    ClusterName = "my-eks-cluster" 
+    PodName     = "backend"
+    Namespace   = "default"
   }
 }
