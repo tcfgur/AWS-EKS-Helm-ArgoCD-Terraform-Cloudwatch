b0e491edb5ff96565804e037291abfd7f9344b38
tcfgur <fatihgur1991@gmail.com>
2024-05-16 22:31:03 +0300

    update rds parameter

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "postgresql_db" {
   storage_type          = "gp2"            
   publicly_accessible   = false               
   db_subnet_group_name  = aws_db_subnet_group.postgresql_subnet_group.name
-  skip_final_snapshot   = false
+  skip_final_snapshot   = true
   iam_database_authentication_enabled = true
 
   username              = jsondecode(data.aws_secretsmanager_secret_version.postgresql_credentials.secret_string)["username"]
